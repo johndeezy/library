4702462a3910ebee5b7427f047b48618397a7dee
John Dow and Kevin Deganos <kevin@>
2016-05-10 09:56:44 -0700

    created adn passed tests for book class

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -20,9 +20,65 @@ public class Book{
   }
 
   public static List<Book> all() {
-    String sql = "SELECT id, title FROM books";
+    String sql = "SELECT id, title FROM books;";
     try(Connection con = DB.sql2o.open()) {
       return con.createQuery(sql).executeAndFetch(Book.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherBook) {
+    if (!(otherBook instanceof Book)) {
+      return false;
+    } else {
+      Book newBook = (Book) otherBook;
+      return this.getTitle().equals(newBook.getTitle()) &&
+            this.getId() == newBook.getId();
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO books (title) VALUES (:title);";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("title", this.title)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static Book find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM books WHERE id=:id;";
+      Book book = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Book.class);
+      return book;
+    }
+  }
+
+  public void update(String title) {
+    this.title = title;
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE books SET title = :title WHERE id = :id;";
+      con.createQuery(sql)
+        .addParameter("title", title)
+        .addParameter("id", this.id)
+        .executeUpdate();
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteQuery = "DELETE FROM books WHERE id = :id;";
+        con.createQuery(deleteQuery)
+          .addParameter("id", this.getId())
+          .executeUpdate();
+
+      String joinDeleteQuery = "DELETE FROM authors_books WHERE book_id = :book_id;";
+        con.createQuery(joinDeleteQuery)
+          .addParameter("book_id", this.getId())
+          .executeUpdate();
     }
   }
 }
--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -24,4 +24,51 @@ public class BookTest {
   public void all_emptyAtFirst() {
     assertEquals(Book.all().size(), 0);
   }
+
+  @Test
+  public void equals_returnsTrueifTitlesAreTheSame() {
+    Book firstBook = new Book("Book 1");
+    Book secondBook = new Book("Book 1");
+    assertTrue(firstBook.equals(secondBook));
+  }
+
+  @Test
+  public void save_returnsTrueifTitlesAreTheSame() {
+    Book firstBook = new Book("Book 1");
+    firstBook.save();
+    assertEquals(Book.all().get(0), firstBook);
+  }
+
+  @Test
+  public void save_assignsIdToObject_1() {
+    Book firstBook = new Book("Book 1");
+    firstBook.save();
+    Book savedBook = Book.all().get(0);
+    assertEquals(savedBook.getId(), firstBook.getId());
+  }
+
+  @Test
+  public void find_findsBookInDatabase_true() {
+    Book myBook = new Book("Book1");
+    myBook.save();
+    Book savedBook = Book.find(myBook.getId());
+    assertTrue(myBook.equals(savedBook));
+  }
+
+  @Test
+  public void update_updatesTitle_String() {
+    Book myBook = new Book("Book 1");
+    myBook.save();
+    myBook.update("Book 2");
+    assertEquals("Book 2", Book.find(myBook.getId()).getTitle());
+  }
+
+  @Test
+  public void delete_deletesBookFromDatabase_true() {
+    Book myBook = new Book("Book 1");
+    myBook.save();
+    int myBookId = myBook.getId();
+    myBook.delete();
+    assertEquals(null, Book.find(myBookId));
+  }
 }
